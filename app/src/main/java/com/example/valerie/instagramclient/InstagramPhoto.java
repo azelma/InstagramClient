5d1b5d365374aa7d23a7a0492cd16d2973797bd1
Valerie Woolard <valerie.woolard@gmail.com>
2015-02-21 17:05:39 -0800

    Basic version of app

---
+++ app/src/main/java/com/example/valerie/instagramclient/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.example.valerie.instagramclient;
+
+/**
+ * Created by valerie on 2/21/15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
---
+++ app/src/main/java/com/example/valerie/instagramclient/InstagramPhotosAdapter.java
@@ -0,0 +1,38 @@
+package com.example.valerie.instagramclient;
+
+import android.content.Context;
+import android.media.Image;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by valerie on 2/21/15.
+ */
+public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
+    public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
+        super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        InstagramPhoto photo = getItem(position);
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+        }
+        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+        tvCaption.setText(photo.username + ": " + photo.caption);
+        ivPhoto.setImageResource(0);
+        Picasso.with(getContext()).load(photo.imageUrl).into(ivPhoto);
+        return convertView;
+    }
+}
